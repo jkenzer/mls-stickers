7558bd5538073d97bfe74fd40925c0f023a77781
jkenzer <jkenzer@gmail.com>
2022-08-11 20:19:11 -0700

    Added better layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,44 @@
 <script>
   import teamlogos from "./assets/teamlogos.json";
-  import Counter from "./lib/Counter.svelte";
 
-  console.log(teamlogos);
+  function shuffleArray(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * i);
+      const temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+    return arr;
+  }
+
+  function random(low, high) {
+    return Math.random() * (high - low) + low;
+  }
 
   const width = window.innerWidth;
   const height = window.innerHeight;
   const favorite = "Portland_Timbers";
 
-  // TODO: Better Spacing
   // TODO: Changable background
   // TODO: More leagues
-  // TODO: Favorite Team
+  // TODO: Favorite Team with special display
   // TODO: Outline the svg instead of the img
-  const stickers = teamlogos.map((logo) => {
-    let xPos = Math.random() * (width - 150);
-    let yPos = Math.random() * (height - 150);
+
+  // 4 rows of 7
+  const colspacing = width / 7;
+  const rowspacing = height / 4;
+  let rowY = -rowspacing;
+
+  const teamLogos = shuffleArray(teamlogos);
+  const stickers = teamLogos.map((logo, idx) => {
+    let xPos = (idx % 7) * colspacing + random(-35, 35);
+    if (idx % 7 == 0) {
+      rowY += rowspacing;
+    }
+    let yPos = rowY + random(-35, 35);
     let zIndex = Math.floor(Math.random() * teamlogos.length);
 
-    if (logo.team == favorite) {
-      zIndex = teamlogos.length + 1;
-      xPos = width / 2 - 75;
-      yPos = height / 2 - 75;
-    }
-    const degRotate = Math.random() * (15 - -15) + -15;
+    const degRotate = random(-15, 15);
     const style = `top:${yPos}px;left:${xPos}px;rotate: ${degRotate}deg;z-index:${zIndex}`;
 
     return {
